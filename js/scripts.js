22358a93cc3f85c631a14c4c02469ff6d1a7b812
Rafael Furry <rfurry@gmail.com>
2017-08-11 11:37:00 -0700

    create logic for ties

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 function check(){
-
         var question1 = document.quiz.question1.value;
         var question2 = document.quiz.question2.value;
         var question3 = document.quiz.question3.value;
+        var question4 = document.quiz.question4.value;
         var languages = [
                 {language: "C#", score: 0},
                 {language: "Java", score: 0},
@@ -11,7 +11,7 @@ function check(){
         ];
         var topScore = 0;
         var answer;
-        var ties = [];
+        var ties = [ ];
 
         if (question1 == "c#") {
                 languages[0].score++;
@@ -43,6 +43,16 @@ function check(){
                 languages[3].score++;
         }
 
+        if (question4 == "c#") {
+                languages[0].score++;
+        } else if (question4 == "java") {
+                languages[1].score++;
+        } else if (question4 == "php") {
+                languages[2].score++;
+        } else if (question4 == "ruby") {
+                languages[3].score++;
+        }
+
         for (var i = 0; i < languages.length; i++) {
                 if(languages[i].score > topScore) {
                         answer = languages[i].language;
@@ -51,16 +61,18 @@ function check(){
         }
 
         for (var i = 0; i < languages.length; i++) {
-                var x = 0;
                 if(topScore == languages[i].score){
-                        ties[x] = languages.language;
-                        x++
+                        ties.push(languages[i].language);
                 }
         }
-        alert(ties.length);
-
 
         document.getElementById("result").style.visibility = "visible";
-        document.getElementById("testResult").innerHTML = "You got" + answer + "!";
+        if (ties.length < 2) {
+                document.getElementById("testResult").innerHTML = "You got" + answer + "!";
+        }
+        else {
+                document.getElementById("testResult").innerHTML = "Yout got a tie!";
+        }
+
 
 }
